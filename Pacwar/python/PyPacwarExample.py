7bf119e6071c5e563898c5eeb84d6bab2ef76a68
Charles Lin <popotwtw@gmail.com>
2021-09-09 22:08:35 -0500

    [New Feature] Modify all 1's to beat all 1's

--- Pacwar/python/PyPacwarExample.py
+++ Pacwar/python/PyPacwarExample.py
@@ -1,18 +1,33 @@
 import _PyPacwar
 import numpy
+import random
 
 
 # Example Python module in C for Pacwar
 def main():
 
     ones = [1] * 50
-    threes = [3] * 50
+    var_ones = [1] * 50
+    #threes = [3] * 50
+    c1 = 100
+    c2 = 100
+    rounds = 500
+    while(c1 > 0 or rounds > 100):
+        var_ones = [1] * 50
+        index_modified=[]
+        for i in range(0, 10):
+            index_modified.append(random.randint(0, 49))
+        for i in index_modified:
+            var_ones[i] = random.randint(0, 3)
+        (rounds, c1, c2) = _PyPacwar.battle(ones, var_ones)
+        print(c1, rounds)
+    print("Found!\n=> ", var_ones)
     print("Example Python module in C for Pacwar")
-    print("all ones versus all threes ...")
-    (rounds, c1, c2) = _PyPacwar.battle(ones, threes)
+    print("all ones versus var all ones ...")
+    #(rounds, c1, c2) = _PyPacwar.battle(ones, threes)
     print("Number of rounds:", rounds)
     print("Ones PAC-mites remaining:", c1)
-    print("Threes PAC-mites remaining:", c2)
+    print("Var PAC-mites remaining:", c2)
 
 
 if __name__ == "__main__":
