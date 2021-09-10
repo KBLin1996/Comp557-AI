01562e350c7fd971d519eb8436454e23ae4a6c23
Charles Lin <popotwtw@gmail.com>
2021-09-09 22:19:34 -0500

    [New Feature] Modify PyPacExample.py to beat all 1's and all 3's

--- Pacwar/python/PyPacwarExample.py
+++ Pacwar/python/PyPacwarExample.py
@@ -8,26 +8,29 @@ def main():
 
     ones = [1] * 50
     var_ones = [1] * 50
-    #threes = [3] * 50
+    threes = [3] * 50
     c1 = 100
     c2 = 100
     rounds = 500
-    while(c1 > 0 or rounds > 100):
+    while(c1 > 0 or c3 > 0):
         var_ones = [1] * 50
         index_modified=[]
         for i in range(0, 10):
             index_modified.append(random.randint(0, 49))
         for i in index_modified:
             var_ones[i] = random.randint(0, 3)
-        (rounds, c1, c2) = _PyPacwar.battle(ones, var_ones)
-        print(c1, rounds)
+        (rounds_1, c1, c2) = _PyPacwar.battle(ones, var_ones)
+        (rounds_2, c3, c2) = _PyPacwar.battle(threes, var_ones)
+        print(c1, rounds_1, c3, rounds_2)
     print("Found!\n=> ", var_ones)
     print("Example Python module in C for Pacwar")
     print("all ones versus var all ones ...")
     #(rounds, c1, c2) = _PyPacwar.battle(ones, threes)
-    print("Number of rounds:", rounds)
+    print("Number of rounds:", rounds_1)
     print("Ones PAC-mites remaining:", c1)
-    print("Var PAC-mites remaining:", c2)
+    #print("Var PAC-mites remaining:", c2)
+    print("Number of rounds:", rounds_2)
+    print("Threes PAC-mites remaining:", c3)
 
 
 if __name__ == "__main__":
