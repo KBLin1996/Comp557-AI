36274f704e87165ade91235263fa446b1242d2cb
Charles Lin <popotwtw@gmail.com>
2021-09-27 23:04:20 -0500

    [New Feature] Q3.2

--- Peeking_BlackJack_MDP/submission.py
+++ Peeking_BlackJack_MDP/submission.py
@@ -30,7 +30,16 @@ def policyEvaluation(mdp, V, pi, epsilon=0.001):
     dictionaries.
     """
     # BEGIN_YOUR_CODE (around 7 lines of code expected)
-    raise Exception("Not implemented yet")
+    mdp.computeStates()
+    for i in range(len(V)):
+        V[i] = 0
+    for i in range(len(V)):
+        all_movement_info = mdp.succAndProbReward(state, pi[state])
+        for newState, prob, reward in all_movement_info:
+            V[state] += prob * (reward + mdp.discount() * V[newState])
+            val_diff = abs(V[newState] - V[state])
+        if val_diff < epsilon:
+            break
     # END_YOUR_CODE
 
 ############################################################
